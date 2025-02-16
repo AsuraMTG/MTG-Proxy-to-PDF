ab6d6b4d9b68e433bd51e89bfae62129874d3b07
Masznyik Márton <eper.karma@gmail.com>
2025-02-16 18:02:20 +0100

    Add project files.

---
+++ MTG Proxy to PDF/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace MTG_Proxy_to_PDF
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnGeneratePdf = new System.Windows.Forms.Button();
+            this.fileName = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnGeneratePdf
+            // 
+            this.btnGeneratePdf.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnGeneratePdf.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.btnGeneratePdf.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnGeneratePdf.Location = new System.Drawing.Point(0, 283);
+            this.btnGeneratePdf.Name = "btnGeneratePdf";
+            this.btnGeneratePdf.Size = new System.Drawing.Size(681, 31);
+            this.btnGeneratePdf.TabIndex = 1;
+            this.btnGeneratePdf.Text = "Generate PDF";
+            this.btnGeneratePdf.UseVisualStyleBackColor = false;
+            this.btnGeneratePdf.Click += new System.EventHandler(this.btnGeneratePdf_Click);
+            // 
+            // fileName
+            // 
+            this.fileName.AutoSize = true;
+            this.fileName.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.fileName.Location = new System.Drawing.Point(7, 24);
+            this.fileName.Margin = new System.Windows.Forms.Padding(6);
+            this.fileName.Name = "fileName";
+            this.fileName.Size = new System.Drawing.Size(75, 16);
+            this.fileName.TabIndex = 2;
+            this.fileName.Text = "File Name: ";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.textBox1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.textBox1.Location = new System.Drawing.Point(91, 21);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(544, 22);
+            this.textBox1.TabIndex = 3;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.fileName);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(681, 65);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(681, 314);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnGeneratePdf);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "MTG Proxy to PDF";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnGeneratePdf;
+        private System.Windows.Forms.Label fileName;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ MTG Proxy to PDF/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using PdfSharp.Pdf;
+using PdfSharp.Drawing;
+
+namespace MTG_Proxy_to_PDF
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.FormBorderStyle = FormBorderStyle.FixedDialog;
+
+            this.MaximizeBox = false;
+
+            this.MinimizeBox = true;
+
+            this.Size = new System.Drawing.Size(500, 125);
+
+            btnGeneratePdf.FlatStyle = FlatStyle.Flat;
+        }
+
+        private void btnGeneratePdf_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderDialog = new FolderBrowserDialog();
+
+            string sourceFolderPath = "";
+            string destinationFolderPath = "";
+
+            if (folderDialog.ShowDialog() == DialogResult.OK)
+            {
+                sourceFolderPath   = folderDialog.SelectedPath;
+                destinationFolderPath = folderDialog.SelectedPath + "/Done";
+            }
+
+            var imageFiles = Directory.GetFiles(sourceFolderPath, "*.png");
+            if (!Directory.Exists(destinationFolderPath))
+            {
+                Directory.CreateDirectory(destinationFolderPath);
+            }
+
+            PdfDocument pdf = new PdfDocument();
+
+            double widthInCm = 6.35;
+            double heightInCm = 8.89;
+
+            double leftMargin = 20;
+            double topMargin = 20;
+
+            double widthInPoints = widthInCm * 28.3465;
+            double heightInPoints = heightInCm * 28.3465;
+
+            int columns = 3;
+            int rows = 3;
+
+            double horizontalSpacing = 10;
+            double verticalSpacing = 10;
+
+            double xPos, yPos;
+            int imageIndex = 0;
+
+            PdfPage page = pdf.AddPage();
+            XGraphics gfx = XGraphics.FromPdfPage(page);
+
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < columns; col++)
+                {
+                    xPos = leftMargin + col * (widthInPoints + horizontalSpacing);
+                    yPos = topMargin + row * (heightInPoints + verticalSpacing);
+
+                    if (imageIndex < imageFiles.Length)
+                    {
+                        XImage image = XImage.FromFile(imageFiles[imageIndex]);
+                        gfx.DrawImage(image, xPos, yPos, widthInPoints, heightInPoints);
+
+                        string destFilePath = Path.Combine(destinationFolderPath, Path.GetFileName(imageFiles[imageIndex]));
+                        File.Move(imageFiles[imageIndex], destFilePath);
+
+                        imageIndex++;
+                    }
+                }
+            }
+
+            while (imageIndex < imageFiles.Length)
+            {
+                pdf.AddPage();
+                page = pdf.Pages[pdf.Pages.Count - 1];
+                gfx = XGraphics.FromPdfPage(page);
+
+                for (int row = 0; row < rows; row++)
+                {
+                    for (int col = 0; col < columns; col++)
+                    {
+                        if (imageIndex < imageFiles.Length)
+                        {
+                            xPos = leftMargin + col * (widthInPoints + horizontalSpacing);
+                            yPos = topMargin + row * (heightInPoints + verticalSpacing);
+
+                            XImage image = XImage.FromFile(imageFiles[imageIndex]);
+                            gfx.DrawImage(image, xPos, yPos, widthInPoints, heightInPoints);
+
+                            string destFilePath = Path.Combine(destinationFolderPath, Path.GetFileName(imageFiles[imageIndex]));
+                            File.Move(imageFiles[imageIndex], destFilePath);
+
+                            imageIndex++;
+                        }
+                    }
+                }
+            }
+
+            string outputFile = Path.Combine(sourceFolderPath, $"{textBox1.Text}.pdf");
+            pdf.Save(outputFile);
+            MessageBox.Show("PDF generálása és képek áthelyezése befejeződött!");
+        }
+    }
+}
