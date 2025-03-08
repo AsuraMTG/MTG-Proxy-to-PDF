634a204a9abd159f7b58105087b1a1554b961e32
Masznyik Márton <eper.karma@gmail.com>
2025-03-08 12:13:33 +0100

    v5.0.0

--- MTG Proxy to PDF/Form1.Designer.cs
+++ MTG Proxy to PDF/Form1.Designer.cs
@@ -38,18 +38,24 @@
             this.buttonDownload = new System.Windows.Forms.Button();
             this.textBoxDeckList = new System.Windows.Forms.TextBox();
             this.groupBoxDeckList = new System.Windows.Forms.GroupBox();
-            this.labelDownloadFinished = new System.Windows.Forms.Label();
             this.labelConvertNumber = new System.Windows.Forms.Label();
             this.textBoxFolderName = new System.Windows.Forms.TextBox();
             this.labelFolderName = new System.Windows.Forms.Label();
+            this.buttonDownloadAndGenerate = new System.Windows.Forms.Button();
+            this.pictureBoxScryFall = new System.Windows.Forms.PictureBox();
+            this.labelScryFall = new System.Windows.Forms.Label();
+            this.labelFolderAndFileName = new System.Windows.Forms.Label();
+            this.textBoxFolderAndFileName = new System.Windows.Forms.TextBox();
             this.groupBoxFileName.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxCard)).BeginInit();
             this.groupBoxDeckList.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxScryFall)).BeginInit();
             this.SuspendLayout();
             // 
             // btnGeneratePdf
             // 
             this.btnGeneratePdf.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnGeneratePdf.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnGeneratePdf.Dock = System.Windows.Forms.DockStyle.Bottom;
             this.btnGeneratePdf.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
             this.btnGeneratePdf.Location = new System.Drawing.Point(0, 420);
@@ -109,16 +115,18 @@
             // 
             this.labelCounter.AutoSize = true;
             this.labelCounter.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.labelCounter.Location = new System.Drawing.Point(274, 398);
+            this.labelCounter.Location = new System.Drawing.Point(273, 360);
             this.labelCounter.Margin = new System.Windows.Forms.Padding(6);
             this.labelCounter.Name = "labelCounter";
-            this.labelCounter.Size = new System.Drawing.Size(0, 16);
+            this.labelCounter.Size = new System.Drawing.Size(51, 16);
             this.labelCounter.TabIndex = 4;
+            this.labelCounter.Text = "Hidden";
             // 
             // buttonDownload
             // 
+            this.buttonDownload.Cursor = System.Windows.Forms.Cursors.Hand;
             this.buttonDownload.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.buttonDownload.Location = new System.Drawing.Point(482, 127);
+            this.buttonDownload.Location = new System.Drawing.Point(276, 329);
             this.buttonDownload.Name = "buttonDownload";
             this.buttonDownload.Size = new System.Drawing.Size(203, 29);
             this.buttonDownload.TabIndex = 9;
@@ -129,12 +137,12 @@
             // textBoxDeckList
             // 
             this.textBoxDeckList.BackColor = System.Drawing.SystemColors.MenuText;
-            this.textBoxDeckList.ForeColor = System.Drawing.SystemColors.Info;
+            this.textBoxDeckList.ForeColor = System.Drawing.SystemColors.ButtonFace;
             this.textBoxDeckList.Location = new System.Drawing.Point(6, 21);
             this.textBoxDeckList.Multiline = true;
             this.textBoxDeckList.Name = "textBoxDeckList";
             this.textBoxDeckList.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.textBoxDeckList.Size = new System.Drawing.Size(188, 288);
+            this.textBoxDeckList.Size = new System.Drawing.Size(397, 166);
             this.textBoxDeckList.TabIndex = 0;
             // 
             // groupBoxDeckList
@@ -143,50 +151,100 @@
             this.groupBoxDeckList.ForeColor = System.Drawing.SystemColors.Control;
             this.groupBoxDeckList.Location = new System.Drawing.Point(277, 74);
             this.groupBoxDeckList.Name = "groupBoxDeckList";
-            this.groupBoxDeckList.Size = new System.Drawing.Size(200, 315);
+            this.groupBoxDeckList.Size = new System.Drawing.Size(405, 193);
             this.groupBoxDeckList.TabIndex = 12;
             this.groupBoxDeckList.TabStop = false;
             this.groupBoxDeckList.Text = "Decklist:";
-            // 
-            // labelDownloadFinished
-            // 
-            this.labelDownloadFinished.AutoSize = true;
-            this.labelDownloadFinished.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.labelDownloadFinished.Location = new System.Drawing.Point(557, 398);
-            this.labelDownloadFinished.Margin = new System.Windows.Forms.Padding(6);
-            this.labelDownloadFinished.Name = "labelDownloadFinished";
-            this.labelDownloadFinished.Size = new System.Drawing.Size(0, 16);
-            this.labelDownloadFinished.TabIndex = 13;
             // 
             // labelConvertNumber
             // 
             this.labelConvertNumber.AutoSize = true;
             this.labelConvertNumber.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.labelConvertNumber.Location = new System.Drawing.Point(480, 165);
+            this.labelConvertNumber.Location = new System.Drawing.Point(480, 360);
             this.labelConvertNumber.Margin = new System.Windows.Forms.Padding(6);
             this.labelConvertNumber.Name = "labelConvertNumber";
-            this.labelConvertNumber.Size = new System.Drawing.Size(0, 16);
+            this.labelConvertNumber.Size = new System.Drawing.Size(51, 16);
             this.labelConvertNumber.TabIndex = 14;
+            this.labelConvertNumber.Text = "Hidden";
             // 
             // textBoxFolderName
             // 
             this.textBoxFolderName.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.textBoxFolderName.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.textBoxFolderName.Location = new System.Drawing.Point(482, 99);
+            this.textBoxFolderName.Location = new System.Drawing.Point(276, 301);
             this.textBoxFolderName.Name = "textBoxFolderName";
-            this.textBoxFolderName.Size = new System.Drawing.Size(204, 22);
+            this.textBoxFolderName.Size = new System.Drawing.Size(203, 22);
             this.textBoxFolderName.TabIndex = 4;
             // 
             // labelFolderName
             // 
             this.labelFolderName.AutoSize = true;
             this.labelFolderName.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.labelFolderName.Location = new System.Drawing.Point(479, 74);
+            this.labelFolderName.Location = new System.Drawing.Point(274, 276);
             this.labelFolderName.Margin = new System.Windows.Forms.Padding(6);
             this.labelFolderName.Name = "labelFolderName";
             this.labelFolderName.Size = new System.Drawing.Size(92, 16);
             this.labelFolderName.TabIndex = 4;
             this.labelFolderName.Text = "Folder Name: ";
+            // 
+            // buttonDownloadAndGenerate
+            // 
+            this.buttonDownloadAndGenerate.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonDownloadAndGenerate.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.buttonDownloadAndGenerate.Location = new System.Drawing.Point(483, 329);
+            this.buttonDownloadAndGenerate.Name = "buttonDownloadAndGenerate";
+            this.buttonDownloadAndGenerate.Size = new System.Drawing.Size(203, 29);
+            this.buttonDownloadAndGenerate.TabIndex = 15;
+            this.buttonDownloadAndGenerate.Text = "Download and  Generate";
+            this.buttonDownloadAndGenerate.UseVisualStyleBackColor = false;
+            this.buttonDownloadAndGenerate.Click += new System.EventHandler(this.buttonDownloadAndGenerate_Click);
+            // 
+            // pictureBoxScryFall
+            // 
+            this.pictureBoxScryFall.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBoxScryFall.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBoxScryFall.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxScryFall.Image")));
+            this.pictureBoxScryFall.Location = new System.Drawing.Point(626, 364);
+            this.pictureBoxScryFall.Name = "pictureBoxScryFall";
+            this.pictureBoxScryFall.Size = new System.Drawing.Size(56, 50);
+            this.pictureBoxScryFall.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxScryFall.TabIndex = 16;
+            this.pictureBoxScryFall.TabStop = false;
+            this.pictureBoxScryFall.Click += new System.EventHandler(this.pictureBoxScryFall_Click);
+            // 
+            // labelScryFall
+            // 
+            this.labelScryFall.AutoSize = true;
+            this.labelScryFall.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.labelScryFall.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.labelScryFall.Location = new System.Drawing.Point(274, 385);
+            this.labelScryFall.Margin = new System.Windows.Forms.Padding(6);
+            this.labelScryFall.Name = "labelScryFall";
+            this.labelScryFall.Size = new System.Drawing.Size(318, 26);
+            this.labelScryFall.TabIndex = 17;
+            this.labelScryFall.Text = "Images provided by scryfall API";
+            // 
+            // labelFolderAndFileName
+            // 
+            this.labelFolderAndFileName.AutoSize = true;
+            this.labelFolderAndFileName.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.labelFolderAndFileName.Location = new System.Drawing.Point(486, 276);
+            this.labelFolderAndFileName.Margin = new System.Windows.Forms.Padding(6);
+            this.labelFolderAndFileName.Name = "labelFolderAndFileName";
+            this.labelFolderAndFileName.Size = new System.Drawing.Size(137, 16);
+            this.labelFolderAndFileName.TabIndex = 18;
+            this.labelFolderAndFileName.Text = "Folder and File name:";
+            // 
+            // textBoxFolderAndFileName
+            // 
+            this.textBoxFolderAndFileName.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.textBoxFolderAndFileName.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.textBoxFolderAndFileName.Location = new System.Drawing.Point(483, 301);
+            this.textBoxFolderAndFileName.Name = "textBoxFolderAndFileName";
+            this.textBoxFolderAndFileName.Size = new System.Drawing.Size(203, 22);
+            this.textBoxFolderAndFileName.TabIndex = 19;
             // 
             // Form1
             // 
@@ -194,10 +252,14 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.ClientSize = new System.Drawing.Size(694, 451);
+            this.Controls.Add(this.pictureBoxScryFall);
+            this.Controls.Add(this.labelConvertNumber);
+            this.Controls.Add(this.labelFolderAndFileName);
+            this.Controls.Add(this.textBoxFolderAndFileName);
+            this.Controls.Add(this.labelScryFall);
+            this.Controls.Add(this.buttonDownloadAndGenerate);
             this.Controls.Add(this.labelFolderName);
             this.Controls.Add(this.textBoxFolderName);
-            this.Controls.Add(this.labelConvertNumber);
-            this.Controls.Add(this.labelDownloadFinished);
             this.Controls.Add(this.groupBoxDeckList);
             this.Controls.Add(this.buttonDownload);
             this.Controls.Add(this.labelCounter);
@@ -212,6 +274,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxCard)).EndInit();
             this.groupBoxDeckList.ResumeLayout(false);
             this.groupBoxDeckList.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxScryFall)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -227,10 +290,14 @@
         private System.Windows.Forms.Button buttonDownload;
         private System.Windows.Forms.TextBox textBoxDeckList;
         private System.Windows.Forms.GroupBox groupBoxDeckList;
-        private System.Windows.Forms.Label labelDownloadFinished;
         private System.Windows.Forms.Label labelConvertNumber;
         private System.Windows.Forms.TextBox textBoxFolderName;
         private System.Windows.Forms.Label labelFolderName;
+        private System.Windows.Forms.Button buttonDownloadAndGenerate;
+        private System.Windows.Forms.PictureBox pictureBoxScryFall;
+        private System.Windows.Forms.Label labelScryFall;
+        private System.Windows.Forms.Label labelFolderAndFileName;
+        private System.Windows.Forms.TextBox textBoxFolderAndFileName;
     }
 }
 
--- MTG Proxy to PDF/Form1.cs
+++ MTG Proxy to PDF/Form1.cs
@@ -19,11 +19,13 @@ using System.Management.Instrumentation;
 using Newtonsoft.Json;
 using MySqlX.XDevAPI;
 using System.Text.RegularExpressions;
+using System.Diagnostics;
 
 namespace MTG_Proxy_to_PDF
 {
     public partial class Form1 : Form
     {
+        private static readonly HttpClient client = new HttpClient();
         public Form1()
         {
             InitializeComponent();
@@ -35,8 +37,9 @@ namespace MTG_Proxy_to_PDF
             this.MinimizeBox = true;
 
             btnGeneratePdf.FlatStyle = FlatStyle.Flat;
+            labelConvertNumber.Text = "";
+            labelCounter.Text = "";
         }
-
         private void btnGeneratePdf_Click(object sender, EventArgs e)
         {
             labelCounter.Text = "";
@@ -156,8 +159,6 @@ namespace MTG_Proxy_to_PDF
             pictureBoxCard.Image = Image.FromFile("Magic_card_back.png");
             textBoxFileName.Text = "";
         }
-
-        private static readonly HttpClient client = new HttpClient();
         public async Task<string> FetchCard(string setCode, string collectorNumber, string path)
         {
             try
@@ -211,7 +212,6 @@ namespace MTG_Proxy_to_PDF
                 return "ERROR";
             }
         }
-
         private async Task DownloadImage(string imageUrl, string savePath)
         {
             try
@@ -234,7 +234,6 @@ namespace MTG_Proxy_to_PDF
         }
         private async void buttonDownload_Click(object sender, EventArgs e)
         {
-            labelDownloadFinished.Text = "";
             labelConvertNumber.Text = "";
 
             FolderBrowserDialog folderDialog = new FolderBrowserDialog();
@@ -305,7 +304,192 @@ namespace MTG_Proxy_to_PDF
                 }
             }
 
-            labelDownloadFinished.Text = "Download Finished!";
+            labelConvertNumber.Text = "Download Finished!";
+        }
+        private async void buttonDownloadAndGenerate_Click(object sender, EventArgs e)
+        {
+            labelConvertNumber.Text = "";
+
+            FolderBrowserDialog folderDialog = new FolderBrowserDialog();
+
+            string sourceFolderPath = "";
+            string destinationFolderPath = "";
+
+            if (folderDialog.ShowDialog() == DialogResult.OK)
+            {
+                sourceFolderPath = folderDialog.SelectedPath;
+                destinationFolderPath = folderDialog.SelectedPath + $"/{textBoxFolderAndFileName.Text}/";
+            }
+            else
+                return;
+
+            if (!Directory.Exists(destinationFolderPath))
+            {
+                Directory.CreateDirectory(destinationFolderPath);
+            }
+
+            string[] cardLines = textBoxDeckList.Lines;
+
+            foreach (var line in cardLines)
+            {
+                if (string.IsNullOrWhiteSpace(line))
+                    continue;
+
+                Match match = Regex.Match(line, @"^(\d+)\s+(.+?)\s*\((\w+)\)\s*(\S+)(?:\s*\*?[A-Za-z]*\*?)?$");
+
+                if (match.Success)
+                {
+                    int count = int.Parse(match.Groups[1].Value);
+
+                    string setCode = match.Groups[3].Value;
+
+                    string collectorNumber = match.Groups[4].Value;
+
+                    setCode = setCode.Replace("★", "").Trim();
+                    collectorNumber = collectorNumber.Replace("★", "").Trim();
+
+                    if (setCode == "PLST" && collectorNumber.Contains("-"))
+                    {
+                    }
+                    else
+                    {
+                        collectorNumber = collectorNumber.Replace("-", "");
+                    }
+
+                    string url = $"https://api.scryfall.com/cards/{setCode}/{collectorNumber}";
+
+                    for (int i = 0; i < count; i++)
+                    {
+                        try
+                        {
+                            string imageFilePath = await FetchCard(setCode, collectorNumber, destinationFolderPath);
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show($"Hiba a kártya letöltésekor: {url}\nRészletek: {ex.Message}", "Letöltési Hiba", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                    }
+
+                    labelConvertNumber.Text = $"{Array.IndexOf(cardLines, line) + 1} / {cardLines.Length}";
+                }
+                else
+                {
+                    MessageBox.Show($"Nem sikerült feldolgozni a sort: {line}", "Hiba", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
+
+            labelConvertNumber.Text = "Download Finished!";
+
+            labelCounter.Text = "";
+
+            if (textBoxFolderAndFileName.Text == "")
+            {
+                MessageBox.Show("Nincs megadva a PDF neve!");
+                return;
+            }
+            sourceFolderPath = sourceFolderPath + $"/{textBoxFolderAndFileName.Text}/";
+            destinationFolderPath = sourceFolderPath + "/PDF";
+
+            var imageFiles = Directory.GetFiles(sourceFolderPath, "*.png");
+
+
+            if (!Directory.Exists(destinationFolderPath))
+            {
+                Directory.CreateDirectory(destinationFolderPath);
+            }
+
+            labelCounter.Text = $"0/{imageFiles.Length}";
+
+            PdfDocument pdf = new PdfDocument();
+
+            double widthInCm = 6.35;
+            double heightInCm = 8.89;
+
+            double leftMargin = 20;
+            double topMargin = 20;
+
+            double widthInPoints = widthInCm * 28.3465;
+            double heightInPoints = heightInCm * 28.3465;
+
+            int columns = 3;
+            int rows = 3;
+
+            double horizontalSpacing = 10;
+            double verticalSpacing = 10;
+
+            double xPos, yPos;
+            int imageIndex = 0;
+
+            PdfPage page = pdf.AddPage();
+            XGraphics gfx = XGraphics.FromPdfPage(page);
+
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < columns; col++)
+                {
+                    xPos = leftMargin + col * (widthInPoints + horizontalSpacing);
+                    yPos = topMargin + row * (heightInPoints + verticalSpacing);
+
+                    if (imageIndex < imageFiles.Length)
+                    {
+                        XImage image = XImage.FromFile(imageFiles[imageIndex]);
+
+                        pictureBoxCard.Image = Image.FromFile(imageFiles[imageIndex]);
+
+                        labelCounter.Text = $"{imageIndex + 1}/{imageFiles.Length}";
+
+                        Application.DoEvents();
+
+                        gfx.DrawImage(image, xPos, yPos, widthInPoints, heightInPoints);
+
+                        imageIndex++;
+                    }
+                }
+            }
+
+            while (imageIndex < imageFiles.Length)
+            {
+                pdf.AddPage();
+                page = pdf.Pages[pdf.Pages.Count - 1];
+                gfx = XGraphics.FromPdfPage(page);
+
+                for (int row = 0; row < rows; row++)
+                {
+                    for (int col = 0; col < columns; col++)
+                    {
+                        if (imageIndex < imageFiles.Length)
+                        {
+                            xPos = leftMargin + col * (widthInPoints + horizontalSpacing);
+                            yPos = topMargin + row * (heightInPoints + verticalSpacing);
+
+
+                            XImage image = XImage.FromFile(imageFiles[imageIndex]);
+
+                            pictureBoxCard.Image = Image.FromFile(imageFiles[imageIndex]);
+
+
+                            labelCounter.Text = $"{imageIndex + 1}/{imageFiles.Length}";
+
+                            Application.DoEvents();
+
+                            gfx.DrawImage(image, xPos, yPos, widthInPoints, heightInPoints);
+
+                            imageIndex++;
+                        }
+                    }
+                }
+            }
+
+            string outputFile = Path.Combine(sourceFolderPath + "/PDF", $"{textBoxFolderAndFileName.Text}.pdf");
+            pdf.Save(outputFile);
+            labelCounter.Text = "PDF generation is Finished!";
+            pictureBoxCard.Image = Image.FromFile("Magic_card_back.png");
+            textBoxFolderAndFileName.Text = "";
+        }
+
+        private void pictureBoxScryFall_Click(object sender, EventArgs e)
+        {
+            Process.Start(new ProcessStartInfo("https://scryfall.com/") { UseShellExecute = true });
         }
     }
 }
